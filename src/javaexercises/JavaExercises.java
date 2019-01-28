7265a86ba8f50e1d3ee18168d732d4034b8a24ba
gismondigm <gismondigm@gmail.com>
2019-01-27 19:26:21 -0500

    added ex 7

--- src/javaexercises/JavaExercises.java
+++ src/javaexercises/JavaExercises.java
@@ -5,16 +5,16 @@ import javaexercises.Exercises.*;
 public class JavaExercises 
 {    
     public static void main(String[] args) {
-        //Exercise1.RunExerciseOne();
-//        Exercise2.RunExerciseTwo();
-//        Exercise3.RunExerciseThree();
-//        Exercise4.RunExerciseFour();
-//        Exercise5.RunExerciseFive();
-//        Exercise6.RunExerciseSix();
-       // Exercise7.RunExerciseSeven();
-       // Exercise8.RunExerciseEight();
+          //Exercise1.RunExerciseOne();
+          //Exercise2.RunExerciseTwo();
+          //Exercise3.RunExerciseThree();
+          //Exercise4.RunExerciseFour();
+          //Exercise5.RunExerciseFive();
+          //Exercise6.RunExerciseSix();
+          //Exercise7.RunExerciseSeven();
+          //Exercise8.RunExerciseEight();
           //Exercise9_1.RunExerciseNine_One();
-          Exercise9_5.RunExerciseNine_Five();
-          //Exercise9_10.RunExerciseNine_Ten();
+          //Exercise9_5.RunExerciseNine_Five();
+        Exercise9_10.RunExerciseNine_Ten();
     }    
 }
